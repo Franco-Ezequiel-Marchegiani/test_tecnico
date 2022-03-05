3b1a9a77e0b9a2307d81d4fd2770ac1fab3d82a5
Franco Ezequiel <eze.cavs@hotmail.com>
2022-03-05 18:38:29 -0300

    Created the button on the text of the page

--- src/containers/informationText/informationText.js
+++ src/containers/informationText/informationText.js
@@ -6,6 +6,7 @@ function InformationText(){
             <span>Jeaper K.Thomas</span>
             <h1>Dunes Anthrazite Black </h1>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At quam, necessitatibus consectetur cum et aliquam delectus aperiam tenetur sequi vero ut! Incidunt ea numquam sapiente rerum deleniti dolorum?</p>
+            <button>Product Details</button>
         </div>
     )
 }
