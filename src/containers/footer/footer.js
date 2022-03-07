cabf4eb771a702a2435a62090ebf3760bf9ca290
Franco Ezequiel <eze.cavs@hotmail.com>
2022-03-07 15:24:48 -0300

    Created the footer component

---
+++ src/containers/footer/footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+import './footer.css'
+function Footer(){
+    return(
+        <footer>
+            <h1>mater</h1>
+        </footer>
+    )
+}
+
+export default Footer;
