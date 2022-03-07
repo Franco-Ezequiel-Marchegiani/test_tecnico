f24e3ba7d7f8cfb38fdc2960aa2cc35b0d2c42de
Franco Ezequiel <eze.cavs@hotmail.com>
2022-03-07 17:19:31 -0300

    Try to add fontawesome but it's not working

--- src/containers/Header/header.js
+++ src/containers/Header/header.js
@@ -1,9 +1,13 @@
 import React from "react";
 import './header.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 function Header(){
     return(
         <header>
             <h1>mater</h1>
+            <p>
+            <FontAwesomeIcon icon="fa-solid fa-bars-sort" />
+            </p>
         </header>
     )
 }
