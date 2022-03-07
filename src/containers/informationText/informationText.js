ebc0695e22c80cc7981940b618bc9e8fc28fb998
Franco Ezequiel <eze.cavs@hotmail.com>
2022-03-07 15:32:12 -0300

    set the classname on every tag

--- src/containers/informationText/informationText.js
+++ src/containers/informationText/informationText.js
@@ -2,11 +2,11 @@ import React from "react";
 import './informationText.css'
 function InformationText(){
     return(
-        <div>
-            <span>Jeaper K.Thomas</span>
-            <h1>Dunes Anthrazite Black </h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At quam, necessitatibus consectetur cum et aliquam delectus aperiam tenetur sequi vero ut! Incidunt ea numquam sapiente rerum deleniti dolorum?</p>
-            <button>Product Details</button>
+        <div className="mainContentContainer">
+            <span className="spanText">Jeaper K.Thomas</span>
+            <h1 className="mainTitle">Dunes Anthrazite Black </h1>
+            <p className="paragraph">Lorem ipsum dolor sit amet consectetur adipisicing elit. At quam, necessitatibus consectetur cum et aliquam delectus aperiam tenetur sequi vero ut! Incidunt ea numquam sapiente rerum deleniti dolorum?</p>
+            <button className="btn-detail">Product Details</button>
             {/* 
             <span>Michael W.Dreeben</span>
             <h1>Shell Dining Chair </h1>
