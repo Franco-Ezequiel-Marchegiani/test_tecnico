d9ca8be64fdac5a71231bc5886513487b5dfdced
Franco Ezequiel <eze.cavs@hotmail.com>
2022-03-07 14:57:54 -0300

    Created the button of the second product

--- src/containers/informationText/informationText.js
+++ src/containers/informationText/informationText.js
@@ -11,7 +11,7 @@ function InformationText(){
             <span>Michael W.Dreeben</span>
             <h1>Shell Dining Chair </h1>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At quam, necessitatibus consectetur cum et aliquam delectus aperiam tenetur sequi vero ut! Incidunt ea numquam sapiente rerum deleniti dolorum?</p>
-            
+            <button>Product Details</button>
              */}
         </div>
     )
